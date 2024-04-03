ff11aee082e42cb701acd6a134c70e7bb6ebefd4
EdoardoMonti07 <edoardo.monti2007@gmail.com>
2024-04-03 18:16:05 +0200

    try 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/InstaWeather/" element={<Homepage />} />
         <Route path="appLayout" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
